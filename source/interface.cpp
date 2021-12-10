074ff0a99361a0953a7f9d2c48b342ffe7d823c7
Kambiz Asadzadeh <kambiz.ceo@gmail.com>
2021-12-11 00:57:47 +0330

    Implement the necessary structures

--- source/interface.cpp
+++ source/interface.cpp
@@ -1,7 +1,43 @@
 #include "interface.hpp"
 
-Interface::Interface()
+namespace CodeMeli {
+
+IdCode::IdCode(QObject* parent)
+    : QObject(parent), m_status(false)
 {
+
+}
+
+
+CodeMeli::IdCode::~IdCode() {}
+
+void IdCode::setIdCode(int code, InsertType type) {
+    //ToDo...
+    bool res{false};
+    setStatus(false);
+}
+
+int IdCode::code() const {
+    return m_code;
+}
+
+bool IdCode::status() const {
+    return m_status;
+}
+
+void IdCode::setCode(int id) {
+    if (m_code == id)
+        return;
+    m_code = id;
+    emit codeChanged(m_code);
+}
+
+void IdCode::setStatus(bool status) {
+    if (m_status == status)
+        return;
+    m_status = status;
+    emit statusChanged(m_status);
+}
 
 }
 
